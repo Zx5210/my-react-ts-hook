0e1fc99959bdba8631ceaa113af25a15975b570e
zx7382 <18268062531@163.com>
2022-10-09 13:27:34 +0800

    feat:创建middleware，配置登录接口

---
+++ __json_server_mock__/middleware.js
@@ -0,0 +1,10 @@
+module.exports = (req, res, next) => {
+	if (req.method === 'POST' && req.path === '/login') {
+		if (req.body.username === 'zx' && req.body.password === '123') {
+			return res.status(200).json({ user: { token: '123456' } })
+		} else {
+			return res.status(400).json({ mag: '用户名或密码错误！' })
+		}
+	}
+	next()
+}
